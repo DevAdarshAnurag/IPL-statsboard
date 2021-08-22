0a686283b6533ef878cac8e369df343a9479c826
Adarsh Anurag <a4adarshanurag@gmail.com>
2021-08-22 17:50:03 +0530

    ballstats class completed

--- src/main/java/com/adarsh/ipl/statsboard/Model/BallStats.java
+++ src/main/java/com/adarsh/ipl/statsboard/Model/BallStats.java
@@ -1,2 +1,156 @@
-package com.adarsh.ipl.statsboard.Model;public class BallStats {
+package com.adarsh.ipl.statsboard.Model;
+
+public class BallStats {
+
+    private long id;
+    private long matchId;
+    private int inning;
+    private int over;
+    private int ball;
+    private String batsman;
+    //private String nonStriker;
+    private String bowler;
+    private int batsmanRuns;
+    private int extraRuns;
+    private int totalRuns;
+    //private int nonBoundary;
+    private int isWicket;
+    private String dismissalKind;
+    private String playerDismissed;
+    private String fielder;
+    private String extrasType;
+    private String battingTeam;
+    private String bowlingTeam;
+
+    public long getId() {
+        return id;
+    }
+
+    public long getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(long matchId) {
+        this.matchId = matchId;
+    }
+
+    public int getInning() {
+        return inning;
+    }
+
+    public void setInning(int inning) {
+        this.inning = inning;
+    }
+
+    public int getOver() {
+        return over;
+    }
+
+    public void setOver(int over) {
+        this.over = over;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public void setBall(int ball) {
+        this.ball = ball;
+    }
+
+    public String getBatsman() {
+        return batsman;
+    }
+
+    public void setBatsman(String batsman) {
+        this.batsman = batsman;
+    }
+
+    public String getBowler() {
+        return bowler;
+    }
+
+    public void setBowler(String bowler) {
+        this.bowler = bowler;
+    }
+
+    public int getBatsmanRuns() {
+        return batsmanRuns;
+    }
+
+    public void setBatsmanRuns(int batsmanRuns) {
+        this.batsmanRuns = batsmanRuns;
+    }
+
+    public int getExtraRuns() {
+        return extraRuns;
+    }
+
+    public void setExtraRuns(int extraRuns) {
+        this.extraRuns = extraRuns;
+    }
+
+    public int getTotalRuns() {
+        return totalRuns;
+    }
+
+    public void setTotalRuns(int totalRuns) {
+        this.totalRuns = totalRuns;
+    }
+
+    public int getIsWicket() {
+        return isWicket;
+    }
+
+    public void setIsWicket(int isWicket) {
+        this.isWicket = isWicket;
+    }
+
+    public String getDismissalKind() {
+        return dismissalKind;
+    }
+
+    public void setDismissalKind(String dismissalKind) {
+        this.dismissalKind = dismissalKind;
+    }
+
+    public String getPlayerDismissed() {
+        return playerDismissed;
+    }
+
+    public void setPlayerDismissed(String playerDismissed) {
+        this.playerDismissed = playerDismissed;
+    }
+
+    public String getFielder() {
+        return fielder;
+    }
+
+    public void setFielder(String fielder) {
+        this.fielder = fielder;
+    }
+
+    public String getExtrasType() {
+        return extrasType;
+    }
+
+    public void setExtrasType(String extrasType) {
+        this.extrasType = extrasType;
+    }
+
+    public String getBattingTeam() {
+        return battingTeam;
+    }
+
+    public void setBattingTeam(String battingTeam) {
+        this.battingTeam = battingTeam;
+    }
+
+    public String getBowlingTeam() {
+        return bowlingTeam;
+    }
+
+    public void setBowlingTeam(String bowlingTeam) {
+        this.bowlingTeam = bowlingTeam;
+    }
 }
