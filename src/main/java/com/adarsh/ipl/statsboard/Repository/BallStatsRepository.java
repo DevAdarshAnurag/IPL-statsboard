8071391322604ef0e950adb32228578804273bc0
Adarsh Anurag <a4adarshanurag@gmail.com>
2021-08-22 18:07:30 +0530

    added ball stat repository

---
+++ src/main/java/com/adarsh/ipl/statsboard/Repository/BallStatsRepository.java
@@ -0,0 +1,7 @@
+package com.adarsh.ipl.statsboard.Repository;
+
+import com.adarsh.ipl.statsboard.Model.BallStats;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BallStatsRepository extends CrudRepository<BallStats, Long> {
+}
