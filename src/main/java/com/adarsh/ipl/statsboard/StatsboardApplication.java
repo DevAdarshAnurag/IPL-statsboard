59a0d60a29a50f1a5d9152684fa3e425e4fd27e5
Adarsh Anurag <a4adarshanurag@gmail.com>
2021-08-22 12:04:51 +0530

    initial commit

---
+++ src/main/java/com/adarsh/ipl/statsboard/StatsboardApplication.java
@@ -0,0 +1,13 @@
+package com.adarsh.ipl.statsboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatsboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adarsh/ipl/statsboard/StatsboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adarsh.ipl.statsboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
