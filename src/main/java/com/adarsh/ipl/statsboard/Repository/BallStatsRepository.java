6038300c38898564e49bff4bfec23111a8b7d7c6
Adarsh Anurag <a4adarshanurag@gmail.com>
2021-08-23 11:12:02 +0530

    Added repository annotation

--- src/main/java/com/adarsh/ipl/statsboard/Repository/BallStatsRepository.java
+++ src/main/java/com/adarsh/ipl/statsboard/Repository/BallStatsRepository.java
@@ -2,6 +2,8 @@ package com.adarsh.ipl.statsboard.Repository;
 
 import com.adarsh.ipl.statsboard.Model.BallStats;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BallStatsRepository extends CrudRepository<BallStats, Long> {
 }
